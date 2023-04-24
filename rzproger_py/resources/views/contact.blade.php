c0a85a96a50980fb657c82d40cbe90a3d8201933
rz <kingradomir@gmail.com>
2023-04-24 09:13:35 +0600

    laravel3

--- rzproger_py/resources/views/contact.blade.php
+++ rzproger_py/resources/views/contact.blade.php
@@ -6,8 +6,36 @@
 
 @section('content')
 <h1>Контакты</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-    Aliquid beatae earum expedita nihil quam suscipit voluptatum!
-    Doloribus id ipsam, minus natus neque nisi perspiciatis?
-    Architecto atque consectetur cum explicabo vero.</p>
+<form action="{{route('contact-form')}}" method="post">
+    @csrf
+    <div class="form-group">
+        <label for="name"></label>
+        <input type="text" name="name" placeholder="введите имя" id="name" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="email"></label>
+        <input type="text" name="email" placeholder="введите email" id="email" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="subject"></label>
+        <input type="text" name="subject" placeholder="введите тему сообщения" id="subject" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="message"></label>
+        <textarea name="message" id="message" class="form-control" placeholder="Введите сообщения" cols="30" rows="10"></textarea>
+    </div>
+    <br>
+    <button type="submit" class="btn btn-success">Отправить</button>
+</form>
+
+
+
+
+{{--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.--}}
+{{--    Aliquid beatae earum expedita nihil quam suscipit voluptatum!--}}
+{{--    Doloribus id ipsam, minus natus neque nisi perspiciatis?--}}
+{{--    Architecto atque consectetur cum explicabo vero.</p>--}}
 @endsection
--- rzproger_py/routes/web.php
+++ rzproger_py/routes/web.php
@@ -5,12 +5,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
-Route::get('/contact', function () {
+Route::get('/feedback', function () {
     return view('contact');
-});
+})->name('contact');
+
+Route::post('/contact/submit', function () {
+    return dd(Request::all());
+//    return "IT IS GOOD";
+})->name('contact-form');
