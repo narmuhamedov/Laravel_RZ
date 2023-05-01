731e8e085cfb1f0a9a83622927e5a8f0367e849a
rz <kingradomir@gmail.com>
2023-05-01 20:14:57 +0600

    'asd'

--- rzproger_py/routes/web.php
+++ rzproger_py/routes/web.php
@@ -15,7 +15,4 @@ Route::get('/feedback', function () {
     return view('contact');
 })->name('contact');
 
-Route::post('/contact/submit', function () {
-    return dd(Request::all());
-//    return "IT IS GOOD";
-})->name('contact-form');
+Route::post('/contact/submit', 'ContactController@submit')->name('contact-form');
